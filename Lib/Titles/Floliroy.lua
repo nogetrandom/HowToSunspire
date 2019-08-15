f1060733cc361b2a68fa57ec746218be8c565591
Floliroy <florian.leeroy@hotmail.com>
2019-08-15 18:53:43 +0200

    add title

--- Lib/Titles/Floliroy.lua
+++ Lib/Titles/Floliroy.lua
@@ -17,3 +17,5 @@ MY_MODULE:RegisterTitle("@Renard7", nil, 92, {en = "Legendary Renard"}, {color={
 MY_MODULE:RegisterTitle("@Renard7", nil, 1330, {en = "Guardian of the Galaxy"}, {color={"#73F9E7", "#D5003F"}})
 MY_MODULE:RegisterTitle("@KarinFlory", nil, 1838, {en = "Tick-Tock Tormentor", de = "Die Tick-Tack-Peinigerin", fr = "Tourmenteuse des Tic-tac"}, {color={"#FF5625", "#2BE5FB"}})
 MY_MODULE:RegisterTitle("@BigBadBlackBonsai", nil, 1391, {en = "Squirts Like a Fire Hose"})
+MY_MODULE:RegisterTitle("@Vatrokion", nil, 92, {en = "SweetHeart"}, {color={"#C71585", "#800080"}})
+MY_MODULE:RegisterTitle("@Targets", nil, true, {en = "|cff0000N|r|cff2a00on|r|cff5500-|r|cff7f00E|r|cffbf00xi|r|cffff00s|r|caaff00t|r|c55ff00i|r|c00ff00n|r|c00ff80g|r |c00ffffP|r|c00aaffa|r|c0055ffr|r|c0000ffs|r|c4600ffe|r|c8b00ffs|r"})
